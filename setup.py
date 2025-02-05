49d353540f4214fd777295014b1fc218e9b01975
jagadeesh760105 <jagadeeshnaikpalthya@gmail.com>
2025-02-05 14:18:47 +0530

    updated

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='jagadeesh naik',
     author_email='jagadeeshnaikpalthya@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2",'langchain_huggingface','langchain-groq==0.1.9'],
     packages=find_packages()
 )
