d4b619f7b023aab6e02b5a0c844f8fefc03dd8f9
jagadeesh760105 <jagadeeshnaikpalthya@gmail.com>
2025-02-12 14:07:48 +0530

    updated code

---
+++ src/mcqgenerator/MCQgenerator.py
@@ -0,0 +1,39 @@
+import os
+import json
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+from src.mcqgenerator.utils import read_file,get_table_data
+from src.mcqgenerator.logger import logging
+
+#imporing necessary packages packages from langchain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+
+from langchain_openai import AzureChatOpenAI
+llm  = AzureChatOpenAI(azure_deployment='gpt-4o',temperature=0)
+
+template="""
+Text:{text}
+You are an expert MCQ maker. Given the above text, it is your job to \
+create a quiz  of {number} multiple choice questions for {subject} students in {tone} tone. 
+Make sure the questions are not repeated and check all the questions to be conforming the text as well.
+Make sure to format your response like  RESPONSE_JSON below  and use it as a guide. \
+Ensure to make {number} MCQs
+### RESPONSE_JSON
+{response_json}
+
+"""
+
+quiz_generation_prompt = PromptTemplate(
+    input_variables=["text", "number", "grade", "tone", "response_json"],
+    template=template)
+
