a98fd47f16aeb89cf35491b161c077af56b5070a
jagadeesh760105 <jagadeeshnaikpalthya@gmail.com>
2025-02-14 14:42:23 +0530

    updated code

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 
 setup(
-    name='mcqgenrator',
+    name='mcqgenerator',
     version='0.0.1',
     author='jagadeesh naik',
     author_email='jagadeeshnaikpalthya@gmail.com',
